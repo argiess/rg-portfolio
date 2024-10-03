665eb92b8dda808ea7b9d9b52b0a1fd04a5e6924
Rg <73758673+argiess@users.noreply.github.com>
2024-10-03 13:27:56 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// BURGER MENU
+// BURGER MENU START
 const burger = document.querySelector(".burger");
 const navMenu = document.querySelector(".list__menu");
 
@@ -11,8 +11,9 @@ document.querySelectorAll(".list__link").forEach(n => n.addEventListener("click"
     burger.classList.remove("active");
     navMenu.classList.remove("active");
 }));
+// BURGER MENU END
 
-// SCROLL EFFECT
+// SCROLL EFFECT START
 const header = document.querySelector(".header");
 
 document.addEventListener("scroll", ()=>{
@@ -28,4 +29,38 @@ document.addEventListener("keydown", ()=>{
     burger.classList.remove("active");
     navMenu.classList.remove("active");
 });
+// SCROLL EFFECT END
 
+// REVIEW SLIDER START
+const swiper = new Swiper('.js-review-slider', {
+    grabCursor: true,
+    spaceBetween: 30,
+    pagination: {
+        el: 'js-reviews-pagination',
+        clickable: true
+    },
+    breakpoints:{
+        767:{
+            slidesPerView: 2
+        }
+    }
+});
+// REVIEW SLIDER END
+
+// FAQ START
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
+// FAQ END
